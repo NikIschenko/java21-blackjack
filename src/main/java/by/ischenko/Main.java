04fbcf2d60c77a6748585659d2809d15f63d9e70
Nikolai Ischenko <nikolaiischenko@coherentsolutions.com>
2024-03-08 18:22:08 +0300

    Main branch initialization

---
+++ src/main/java/by/ischenko/Main.java
@@ -0,0 +1,12 @@
+package by.ischenko;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+  public static void main(String[] args) {
+
+    log.warn("Find the description for the project in README.md file in the root 😉");
+  }
+}
